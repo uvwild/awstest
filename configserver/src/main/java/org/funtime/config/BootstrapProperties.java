d7fcf9119a528501100e2a962dcca7e6a5f6221e
uv.wildner <uv.wildner@idealo.de>
2016-01-25 01:53:36 +0100

    MS3 tomcat WAR deployment using cargo to running local instance working

--- configserver/src/main/java/org/funtime/config/BootstrapProperties.java
+++ configserver/src/main/java/org/funtime/config/BootstrapProperties.java
@@ -2,18 +2,24 @@ package org.funtime.config;
 
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
+import org.springframework.beans.factory.config.YamlPropertiesFactoryBean;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Component;
 
+import javax.annotation.PostConstruct;
 
-//@Profile("local")
+
 @Component
 @Configuration
 @ConfigurationProperties(locations = "classpath:/bootstrap.yml", ignoreInvalidFields = true, ignoreUnknownFields = true)
-@Getter @Setter
+@Getter
+@Setter
 public class BootstrapProperties {
 
     private String dummy;
@@ -34,12 +40,29 @@ public class BootstrapProperties {
         private String contextPath;
     }
 
+
+    @Autowired
+    ConfigurableListableBeanFactory beanFactory;
+
     /**
      * Property placeholder configurer needed to process @Value annotations
      */
     @Bean
-    public static PropertySourcesPlaceholderConfigurer propertyConfigurer() {
-        return new PropertySourcesPlaceholderConfigurer();
+    public PropertySourcesPlaceholderConfigurer propertyConfigurer() {
+        PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer = new PropertySourcesPlaceholderConfigurer();
+        YamlPropertiesFactoryBean yaml = new YamlPropertiesFactoryBean();
+        yaml.setResources(new ClassPathResource("bootstrap.yml"));
+        propertySourcesPlaceholderConfigurer.setProperties(yaml.getObject());
+//        propertySourcesPlaceholderConfigurer.postProcessBeanFactory(beanFactory);
+//        properties = propertySourcesPlaceholderConfigurer.getAppliedPropertySources().get(PropertySourcesPlaceholderConfigurer.LOCAL_PROPERTIES_PROPERTY_SOURCE_NAME);
+        return propertySourcesPlaceholderConfigurer;
     }
+
+    @PostConstruct
+    public void init() {
+        System.out.println("================== dummy: " + dummy + "================== ");
+    }
+
+
 }
 
--- configserver/src/main/java/org/funtime/config/ConfigServerApplication.java
+++ configserver/src/main/java/org/funtime/config/ConfigServerApplication.java
@@ -1,33 +1,58 @@
 package org.funtime.config;
 
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerFactory;
+import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainerFactory;
 import org.springframework.boot.context.web.SpringBootServletInitializer;
+import org.springframework.boot.test.ConfigFileApplicationContextInitializer;
 import org.springframework.cloud.config.server.EnableConfigServer;
-import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
+import org.springframework.test.context.ContextConfiguration;
 
 import java.util.Set;
+import java.util.concurrent.TimeUnit;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  * Created by uv on 10/12/2015 for awstest
  * SpringBootServletInitializer added to run this in a eb container
  */
-//@Configuration
-@ComponentScan
-@EnableAutoConfiguration
+@Profile("default")
+@ContextConfiguration(initializers = ConfigFileApplicationContextInitializer.class)
+@SpringBootApplication
 @EnableConfigServer
 public class ConfigServerApplication extends SpringBootServletInitializer
 {
+    static Logger LOG = Logger.getLogger(ConfigServerApplication.class.getName());
 
     @Override
     protected SpringApplicationBuilder configure (SpringApplicationBuilder builder){
-        return builder.sources(ConfigServerApplication.class, BootstrapProperties.class, StatusController.class);
+        LOG.log(Level.FINE, "configure " + builder);
+        return builder.sources(BootstrapProperties.class, ConfigServerApplication.class, StatusController.class);
     }
 
     public static void main(String[] args) {
+        LOG.log(Level.FINE, "main");
+
         SpringApplicationBuilder springApplicationBuilder = new SpringApplicationBuilder(ConfigServerApplication.class);
 //                .properties("spring.config.name=configserver")
         Set<Object> source = springApplicationBuilder.application().getSources();
         springApplicationBuilder.run(args);
     }
+
+    @Bean
+    public EmbeddedServletContainerFactory servletContainer() {
+
+        TomcatEmbeddedServletContainerFactory factory = new TomcatEmbeddedServletContainerFactory();
+        int port = 8080;
+        LOG.log(Level.FINE, "port: " + port);
+        factory.setPort(port);
+        factory.setSessionTimeout(10, TimeUnit.MINUTES);
+        //factory.addErrorPages(new ErrorPage(HttpStatus.404, "/notfound.html"));
+        return factory;
+    }
+
 }
